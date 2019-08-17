49ed0d1a97b690a552e4ab667cb7f8edfb906a90
Try <khovct@gmail.com>
2019-08-17 12:53:01 -0700

    downloaded react-navigation react-gesture-handler firebase and incorporated firebase into App.js

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+
+let firebaseConfig = { // from the firebase website after clicking on the </> button
+  apiKey: "AIzaSyC1AC08ZnST0_R3ToAxGBZ0b8Ma-clcLds",
+  authDomain: "reactbootcamp-fab67.firebaseapp.com",
+  databaseURL: "https://reactbootcamp-fab67.firebaseio.com",
+  projectId: "reactbootcamp-fab67",
+  storageBucket: "",
+  messagingSenderId: "897342952156",
+  appId: "1:897342952156:web:b74577d2a19c40af"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
