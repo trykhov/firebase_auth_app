55084aad03ae5de90afbd344eeb175f6806806c5
Try <khovct@gmail.com>
2019-08-22 18:47:58 -0700

    designed the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import * as firebase from 'firebase';
+import { Button } from 'native-base';
 
 
 export default class HomeScreen extends React.Component {
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Home Screen</Text>
-            </View>
-            )
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: ""
     }
+  };
+
+  // componentDidMount() {
+  //   this.setState()
+  // }
+
+  render() {
+      return (
+          <View style={styles.container}>
+              <View style={styles.logoContainer}>
+                <Image source={require('../assets/logo.png')} />
+                <Text>LearnCodeOnline.in</Text>
+              </View>
+              <View style={styles.userDetails}>
+                <Text>Hey {this.state.name}</Text>
+                <Text>You are signed in as: {this.state.email}</Text>
+              </View>
+              <Button style={styles.button}
+                full 
+                rounded 
+                success 
+                onPres={() => {}}
+              >
+                <Text style={styles.buttonText}>Sign Out</Text>
+              </Button>
+          </View>
+      ) 
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    margin: 20
   },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 100,
+    marginBottom: 100
+  },
+  userDetails: {},
+
+  button: {
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#fff"
+  }
 });
